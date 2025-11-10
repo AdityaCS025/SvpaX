e6b564dc6c66c6721759224a53e9910c2923c2c2
AdityaCS025 <adityaworld2006@gmail.com>
2025-11-10 14:02:49 +0530

    Initial commit for SVPAX project

--- backend/routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { testGeminiAPI } = require('../controllers/test');
-
-// Test route for Gemini API
-router.get('/gemini', testGeminiAPI);
-
-module.exports = router;
